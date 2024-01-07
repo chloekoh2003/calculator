44eef8c81ba88200de3749cad77b5ef5b711e5f2
chloekoh2021 <chloe.koh.2021@scis.smu.edu.sg>
2024-01-08 01:33:15 +0800

    added button effects

--- src/app/page.js
+++ src/app/page.js
@@ -30,33 +30,38 @@ export default function Home() {
   ];
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold mb-10">Calculator</h1>
-      <div className="bg-white p-6 rounded-lg shadow-lg">
-        <input
-          type="text"
-          className="w-full mb-2 text-3xl border-b-2 border-gray-600"
-          value={expression}
-          readOnly
-        />
-        <input 
-          type="text" 
-          className="w-full text-4xl font-bold mb-4"
-          value={result}
-          readOnly
-        />
-        <div className="grid grid-cols-4 gap-2">
-          {buttons.map((btn) => (
-            <button
-              key={btn}
-              onClick={() => handleButtonClick(btn)}
-              className="text-4xl bg-gray-300 hover:bg-gray-400 p-2 rounded-lg"
-            >
-              {btn}
-            </button>
-          ))}
+    <div className="flex justify-center min-h-screen">
+      <main className="flex flex-col p-24 w-6/12">
+        <h1 className="text-4xl font-bold mb-10 text-left">Calculator</h1>
+        <div className="bg-white p-6 rounded-lg shadow-lg">
+          <input 
+            type="text" 
+            className="w-full text-4xl border-b-2 border-gray-200 font-bold mb-2"
+            value={result}
+            readOnly
+          />
+          <input
+            type="text"
+            className="w-full mb-2 text-md"
+            value={expression}
+            readOnly
+          />
+          <div className="grid grid-cols-4 gap-2">
+            {buttons.map((btn) => (
+              <button
+                key={btn}
+                onClick={() => handleButtonClick(btn)}
+                className={`text-4xl p-2 rounded-lg shadow-lg ${
+                  ['/', '*', '-', '+', '='].includes(btn) ? 'operation-button' : 
+                  btn ==='C' ? 'clear-button': 'bg-gray-300 num-button'
+                }`}
+              >
+                {btn}
+              </button>
+            ))}
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   )
 }
